eaaad7edafdca0d5102884c5abbf6b975ad9ec36
Alan Jordan <alanpjordan@gmail.com>
2017-04-28 12:20:57 +1200

    Passed tests to check if cell is resurrectable

--- overPopulated.js
+++ overPopulated.js
@@ -1,5 +1,5 @@
 function overPopulated(getNeighbour) {
-
+  return getNeighbour > 3 ? true : false
 }
 
 module.exports = overPopulated
---
+++ resurrectable.js
@@ -0,0 +1,5 @@
+function resurrectable(getNeighbour) {
+  return getNeighbour == 3 ? true : false
+}
+
+module.exports = resurrectable
