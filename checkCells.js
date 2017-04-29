423d9f172c5d56323d8af94c11cb144c61a950a7
Alan Jordan <alanpjordan@gmail.com>
2017-04-29 12:23:02 +1200

    Pushing to github to work on another machine

--- checkCells.js
+++ checkCells.js
@@ -1,18 +1,14 @@
 var getNeighbours = require('./getNeighbours')
 
 function checkCells(board) {
-  var arr = []
-  var test = []
+  var allCells = []
   for (var i = 0; i < board.length; i++) {
     for (var j = 0; j < board.length; j++) {
-      // console.log("[" + i + "][" + j + "] = " + board[i][j]);
       tempArr = [i, j]
-      test.push("My neighbours are....")
-      test.push(getNeighbours(tempArr, board.length))
-      test.push("---------------")
+      allCells.push(tempArr)
     }
   }
-  console.log(test.length)
+  console.log(allCells)
   return arr
 }
 module.exports = checkCells
