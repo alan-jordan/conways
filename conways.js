126b0356905fb3c41e8a5a750ab0271f11768051
Alan Jordan <alanpjordan@gmail.com>
2017-04-30 08:39:19 +1200

    Stopping first render

--- conways.js
+++ conways.js
@@ -1,14 +1,4 @@
-//generates initial board
-//loops through each cell on the board and works out
-  // Number of neighbours
-  // Neighbours out of bounds
-  // neighbours status, counts trues
-  // Checks the next state of that cell
-  // Updates the cell if required.
-// Once at end of loop, displays next board
-  // Then Performs loop through to check next cell state
 var clear = require('clear');
-
 var createBoard = require('./createBoard')
 var checkCells = require('./checkCells')
 var getNeighbours = require('./getNeighbours')
@@ -17,8 +7,6 @@ var renderBoard = require('./renderBoard')
 
 var currentBoard = createBoard(50)
 var cellsArr = checkCells(currentBoard)
-clear()
-renderBoard(currentBoard)
 
 let timerId = setTimeout(function delay(){
   clear()
