321fce58132a0cb9463712cc7e665127fe35e744
Alan Jordan <alanpjordan@gmail.com>
2017-05-01 13:54:57 +1200

    Refactored checkNextCellSTatus

--- checkNextCellStatus.js
+++ checkNextCellStatus.js
@@ -2,12 +2,12 @@ function checkNextCellStatus(cells, board) {
   var nextBoard = cells.map(function(cell){
     var row = cell.cellPos[0]
     var col = cell.cellPos[1]
-    if(board[row][col] == false){
+    if(!board[row][col]){
       cell.resurrectable ? board[row][col] = true : false
-    } else if (board[row][col] == true){
+    } else if (board[row][col]){
       cell.underPopulated ? board[row][col] = false : false
       cell.overPopulated ? board[row][col] = false : false
-      cell.staysAlive && cell.aliveCount == 2 ? board[row][col] = true : false
+      cell.staysAlive ? board[row][col] = true : false
     }
   })
   return board
